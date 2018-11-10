81f62825305b1df65ecd16740a10049e70b85024
HypoSoc <hyposoc1@gmail.com>
2018-11-10 17:55:53 -0500

    Delusion Quality of Life hotfix.

--- src/main/java/gluttonmod/actions/DelusionAction.java
+++ src/main/java/gluttonmod/actions/DelusionAction.java
@@ -6,9 +6,12 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
+import java.util.ArrayList;
+
 public class DelusionAction extends AbstractGameAction {
     private AbstractPlayer p;
-    private static String[] TEXT = {"Choose a card to echo. You cannot echo ethereal cards."};
+    private static String[] TEXT = {"Choose a card to echo."};
+    private ArrayList<AbstractCard> etherealCards = new ArrayList();
 
     public DelusionAction(AbstractPlayer p, int amount){
         this.p = p;
@@ -19,35 +22,59 @@ public class DelusionAction extends AbstractGameAction {
 
     public void update() {
         if(this.duration == Settings.ACTION_DUR_FAST) {
-            if (this.p.hand.size() == 0) {
+            for (AbstractCard c : this.p.hand.group) {
+                if (c.isEthereal) {
+                    this.etherealCards.add(c);
+                }
+            }
+            if (this.p.hand.size() == this.etherealCards.size()) {
                 this.isDone = true;
                 return;
             }
-            if (this.p.hand.size() == 1) {
-                AbstractCard c = this.p.hand.getTopCard();
-                if (!c.isEthereal) {
-                    AbstractDungeon.actionManager.addToBottom(
-                            new MakeEchoAction(c, this.amount)
-                    );
+            if (this.p.hand.size() - this.etherealCards.size() == 1) {
+                for (AbstractCard c : this.p.hand.group) {
+                    if (!c.isEthereal) {
+                        AbstractDungeon.actionManager.addToBottom(
+                                new MakeEchoAction(c, this.amount));
+                        this.isDone = true;
+                        return;
+                    }
                 }
-            } else {
-                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false, false);
+            }
+            this.p.hand.group.removeAll(this.etherealCards);
+            if (this.p.hand.group.size() > 1)
+            {
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false, false, false, true);
                 tickDuration();
                 return;
+            }
+            if (this.p.hand.group.size() == 1)
+            {
+                returnCards();
+                AbstractDungeon.actionManager.addToBottom(
+                        new MakeEchoAction(this.p.hand.getTopCard(), this.amount));
+                this.isDone = true;
             }
         }
         if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved)
         {
+            returnCards();
             for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
-                if (!c.isEthereal) {
-                    AbstractDungeon.actionManager.addToBottom(
-                            new MakeEchoAction(c, this.amount)
-                    );
-                }
+                AbstractDungeon.actionManager.addToBottom(
+                        new MakeEchoAction(c, this.amount));
                 p.hand.addToTop(c);
             }
             AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+            AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+            this.isDone = true;
         }
         tickDuration();
+    }
+
+    private void returnCards() {
+        for (AbstractCard c : this.etherealCards) {
+            this.p.hand.addToTop(c);
+        }
+        this.p.hand.refreshHandLayout();
     }
 }
