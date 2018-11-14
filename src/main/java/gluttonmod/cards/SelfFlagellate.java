eb23d8125671983ab7811bab42a975a2ab5da2db
HypoSoc <hyposoc1@gmail.com>
2018-11-13 23:55:07 -0500

    Buffed Self Flagellate

--- src/main/java/gluttonmod/cards/SelfFlagellate.java
+++ src/main/java/gluttonmod/cards/SelfFlagellate.java
@@ -23,7 +23,7 @@ public class SelfFlagellate extends AbstractGluttonCard
     private static final CardTarget TARGET = CardTarget.SELF;
 
     private static final int COST = 1;
-    private static final int MAGIC = 1;
+    private static final int MAGIC = 2;
     private static final int UPGRADE_MAGIC_BONUS = 1;
 
     public SelfFlagellate()
