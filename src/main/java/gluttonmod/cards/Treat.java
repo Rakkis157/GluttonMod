9b034a20467040651f3eb2244c48642123362ace
HypoSoc <hyposoc1@gmail.com>
2018-11-18 21:08:28 -0500

    Buffed Treat. Eventually, I will get it to a proper number.

--- src/main/java/gluttonmod/cards/Treat.java
+++ src/main/java/gluttonmod/cards/Treat.java
@@ -19,7 +19,7 @@ public class Treat extends AbstractGluttonCard
 
     private static final int COST = 1;
     private static final int BLOCK = 8;
-    private static final int MAGIC = 1;
+    private static final int MAGIC = 2;
     private static final int UPGRADE_BONUS = 2;
 
     public Treat()
